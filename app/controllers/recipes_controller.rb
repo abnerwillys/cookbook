d71afb6c5d0eebbd16a13f31b558a8b1c4f25b2c
Abner Willys <abwillys001@gmail.com>
2022-04-25 14:53:16 -0300

    edit forms and componetization using ERB

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -24,4 +24,25 @@ class RecipesController < ApplicationController
 
     render :new
   end
+
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+
+    success = @recipe.update(
+      name: params[:recipe][:name],
+      recipe_type_id: params[:recipe][:recipe_type_id],
+      cuisine: params[:recipe][:cuisine],
+      ingredients: params[:recipe][:ingredients],
+      cook_method: params[:recipe][:cook_method],
+      cook_time: params[:recipe][:cook_time],
+    )
+
+    return redirect_to recipe_path(@recipe.id) if success
+
+    render :edit
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'home#index'
 
-  resources :recipes, only: [:show, :create, :new]
+  resources :recipes, only: [:show, :create, :new, :edit, :update]
   resources :recipe_types, only: [:create, :new]
 end
